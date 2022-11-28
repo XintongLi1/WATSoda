d519677bc685fb2bf5d06ab3ce603790fcf7f1fa
XintongLi1 <1720731919@qq.com>
2022-11-28 17:46:46 -0500

    rebase, merge parent ..

--- parent.cc
+++ parent.cc
@@ -1,22 +1,32 @@
 #include "parent.h"
+#include <uPRNG.h>
 
 Parent::Parent( Printer & prt, Bank & bank, unsigned int numStudents, unsigned int parentalDelay ) : prt(prt), bank(bank), numStudents(numStudents), parentalDelay(parentalDelay) {}
 
 void Parent::main() {
+    PRNG prng;
+    // print start message
+    prt.print(Printer::Parent, 'S');
+
     for (;;) {
         // yielding busy wait to destructor
-        _Accept(~Parent) break;
+        _Accept(~Parent) {
+            // print finish message
+            prt.print(Printer::Parent, 'F');
+            return;
+        }
         _Else {
             // each gift has to delay first
             yield(parentalDelay);
 
-            // give between 1 - 3 inclusive to a random student
-            unsigned int student_id = prng(0, numStudents), gift = prng(1, 4);
+            // give between 1 - 3 inclusive
+            unsigned int student_id = prng(numStudents), gift = prng(1, 3);
 
             // gift student
             bank.deposit(student_id, gift);
 
-            prt.print( Printer::Kind::Parent, 'D', id, amount );
+            // print deposit gift message
+            prt.print(Printer::Parent, 'D', student_id, gift);
         }
     }
     prt.print(Printer::Kind::Parent, 'F'); 
